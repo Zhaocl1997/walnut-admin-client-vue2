94e99b66013314371d8e7ca09cca12e74b9df12a
zhaocl1997 <674351624@qq.com>
2020-11-17 16:55:21 +0800

    fix(input): fix import location

--- src/components/UI/Base/Input/index.vue
+++ src/components/UI/Base/Input/index.vue
@@ -93,8 +93,8 @@
 </template>
 
 <script>
-import BlockMixins from "@/mixins/Block";
-import ValueMixins from "@/mixins/Value";
+import BlockMixins from "../utils/mixins/block";
+import ValueMixins from "../utils/mixins/value";
 
 import {
   isEmpty,
