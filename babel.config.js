5338c7f9ea68483aec44b762615308a41bb75ef8
zhaocl1997 <674351624@qq.com>
2020-12-21 11:03:57 +0800

    chore: add prettier

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    // 按需引入配置
-    plugins: [
-        [
-            "component",
-            {
-                "libraryName": "element-ui",
-                "styleLibraryName": "theme-chalk"
-            }
-        ]
-    ]
+  // 按需引入配置
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk',
+      },
+    ],
+  ],
 }
