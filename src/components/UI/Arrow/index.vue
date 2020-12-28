01ebd6f456a8b655db49e90f94aacc151aaad8b9
zhaocl1997 <674351624@qq.com>
2020-12-28 16:50:37 +0800

    feat: add Title and Arrow components

---
+++ src/components/UI/Arrow/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <w-icon
+    icon="baselineKeyboardArrowDown"
+    width="24"
+    pointer
+    :class="getClass"
+  ></w-icon>
+</template>
+
+<script>
+  import { ref, reactive, computed, defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'WArrow',
+
+    props: {
+      active: Boolean,
+    },
+
+    setup(props, { attrs }) {
+      const getClass = computed(() => {
+        const { active } = props
+        if (typeof active === 'boolean') {
+          return [
+            'w-arrow',
+            {
+              'w-arrow--active': active,
+            },
+          ]
+        }
+        return 'u-none'
+      })
+
+      return {
+        getClass,
+      }
+    },
+  })
+</script>
+
+<style lang="scss" scoped>
+  .w-arrow {
+    display: inline-block;
+    transform: rotate(0deg);
+    transition: all 0.3s ease 0.1s;
+    transform-origin: center center;
+
+    &--active {
+      transform: rotate(180deg);
+    }
+  }
+</style>
---
+++ src/components/UI/Title/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="w-title">
+    <div class="w-title__text">{{ text }}</div>
+  </div>
+</template>
+
+<script>
+  import { ref, reactive, computed, defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'WTitle',
+
+    props: {},
+
+    setup(props, { attrs, slots }) {
+      const text = slots.default()[0].children
+
+      return {
+        text,
+      }
+    },
+  })
+</script>
+
+<style lang="scss" scoped>
+  .w-title {
+    height: 30px;
+    line-height: 30px;
+  }
+
+  .w-title__text {
+    padding-bottom: 10px;
+    position: relative;
+    padding-left: 15px;
+    font-size: 16px;
+    font-weight: 700;
+    user-select: none;
+  }
+
+  .w-title__text:before {
+    content: ' ';
+    border-left: 4px solid orangered;
+    display: inline-block;
+    height: 16px;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    margin-top: -13px;
+  }
+</style>
