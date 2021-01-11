32c0a2c57cc67a9f5ccb1e3c2d2f4cd809228da8
zhaocl1997 <674351624@qq.com>
2021-01-11 21:52:56 +0800

    feat: use vite new feat `import.meta.glob` to rewrite locale getMessage function

---
+++ src/locales/utils.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import { set } from 'lodash-es'
+
+const modules = import.meta.globEager('./lang/**/*.js')
+
+function genMessage(langs) {
+  const obj = {}
+
+  Object.keys(langs).forEach((key) => {
+    const mod = langs[key].default
+    let k = key.replace(`./lang/`, '').replace(/^\.\//, '')
+    const lastIndex = k.lastIndexOf('.')
+    k = k.substring(0, lastIndex)
+    const keyList = k.split('/')
+    const lang = keyList.shift()
+    const objKey = keyList.join('.')
+    if (lang) {
+      set(obj, lang, obj[lang] || {})
+      set(obj[lang], objKey, mod)
+    }
+  })
+
+  return obj
+}
+
+export default genMessage(modules)
