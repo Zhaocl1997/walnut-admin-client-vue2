7420e1b935009ae9422f30f2d9c3f894974c254b
zhaocl1997 <674351624@qq.com>
2021-01-11 21:49:32 +0800

    chore: refactor logic in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+'use strict'
+
+import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { loadEnv } from './src/utils/env'
-import globbyTransform from './build/vite/globby/index.js'
 
 function pathResolve(dir) {
   return resolve(__dirname, '.', dir)
@@ -8,54 +10,26 @@ function pathResolve(dir) {
 
 const root = process.cwd()
 const alias = {
-  '/@/': pathResolve('src'),
+  '/@/': `${pathResolve('src')}/`,
 }
-const resolvers = []
 
 const { VITE_PORT } = loadEnv()
 
-module.exports = {
-  root,
-  alias,
+export default ({ command, mode }) => {
+  return {
+    root,
+    alias,
 
-  /**
-   * @description port
-   * @default 80
-   */
-  port: VITE_PORT,
+    plugins: [vue()],
 
-  /**
-   * @description
-   * Configure dep optimization behavior.
-   */
-  optimizeDeps: {
-    include: [
-      'element-plus/lib/locale/lang/zh-cn',
-      'element-plus/lib/locale',
-      'mockjs',
-    ],
-  },
+    port: VITE_PORT,
 
-  proxy: {
-    // 代理
-    // // string shorthand
-    // "/foo": "http://localhost:4567/foo",
-    // // with options
-    // "/api": {
-    //     target: "http://jsonplaceholder.typicode.com",
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ""),
-    // },
-  },
-
-  plugins: [],
-
-  transforms: [
-    globbyTransform({
-      resolvers: resolvers,
-      root: root,
-      alias: alias,
-      includes: [resolve('src/router'), resolve('src/locales')],
-    }),
-  ],
+    optimizeDeps: {
+      include: [
+        'element-plus/lib/locale/lang/zh-cn',
+        'element-plus/lib/locale',
+        'mockjs',
+      ],
+    },
+  }
 }
