29b00190bbb634f77d78a0703477f874b80579a4
zhaocl1997 <674351624@qq.com>
2021-02-14 15:44:31 +0800

    chore: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,52 @@
 'use strict'
 
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { loadEnv } from './src/utils/env'
 
 const { VITE_PORT, VITE_PROXY, VITE_API_PREFIX, VITE_API_VERSION } = loadEnv()
 const apiPrefix = VITE_API_PREFIX + VITE_API_VERSION
 
-const root = process.cwd()
-const alias = {
-  '/@/': `${require('path').resolve(__dirname, '.', 'src')}/`,
-}
-const server = {
-  port: VITE_PORT,
-
-  proxy: {
-    [apiPrefix]: {
-      target: VITE_PROXY,
-      changeOrigin: true,
-      ws: true,
-      rewrite: (path) => path.replace(new RegExp(`^${apiPrefix}`), ''),
-    },
-  },
-
-  hmr: {
-    overlay: true,
-  },
-
-  open: '/auth',
+const resolvePath = (dir) => {
+  return resolve(__dirname, '.', dir)
 }
 
 export default ({ command, mode }) => {
+  const root = process.cwd()
+
+  const alias = [
+    {
+      // /@/xxxx  =>  src/xxx
+      find: /^\/@\//,
+      replacement: resolvePath('src') + '/',
+    },
+  ]
+
+  const server = {
+    port: VITE_PORT,
+
+    proxy: {
+      [apiPrefix]: {
+        target: VITE_PROXY,
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(new RegExp(`^${apiPrefix}`), ''),
+      },
+    },
+
+    hmr: {
+      overlay: true,
+    },
+
+    open: '/auth',
+  }
+
   return {
     root,
 
-    alias,
+    resolve: {
+      alias,
+    },
 
     server,
 
